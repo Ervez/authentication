b021881f1ba582f6f0d23af770a6f5d084934a69
Ervez <fb.blaszkiewicz@gmail.com>
2023-03-19 15:44:24 +0100

    nabar&homepage cdn

--- Authentication/userauth/views.py
+++ Authentication/userauth/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+
 def HomePage(request):
-    pass
+    return render(request, 'templates/home.html')
+
+
 def SignupPage(request):
     pass
+
+
 def LoginPage(request):
     pass
+
+
+def home(request):
+    return render(request, 'userauth/home.html')
