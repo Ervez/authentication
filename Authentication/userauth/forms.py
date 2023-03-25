55711eec08b3fa6191770282a06a37a6fcf2bbda
Ervez <fb.blaszkiewicz@gmail.com>
2023-03-25 15:57:17 +0100

    register form cd

--- Authentication/userauth/forms.py
+++ Authentication/userauth/forms.py
@@ -4,9 +4,20 @@ from django import forms
 
 
 class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(max_length=20, label="Nazwa użytkownika")
+    password1 = forms.CharField(
+        label="Hasło",
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label="Potwierdź hasło",
+        strip=False,
+        widget=forms.PasswordInput,
+    )
     email = forms.EmailField()
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
+    first_name = forms.CharField(max_length=50, label="Imię")
+    last_name = forms.CharField(max_length=50, label="Nazwisko")
 
     class Meta:
         model = User
